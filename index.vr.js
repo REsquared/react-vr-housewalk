60458fe6f6165386846041ebecda6b45c4c0df13
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-05-19 21:19:57 +0400

    change position of user when he clicks buttons

--- index.vr.js
+++ index.vr.js
@@ -17,11 +17,19 @@ observer(class react_vr_housewalk extends React.Component {
   render() {
     const {index, button_positions, current_position} = global_store;
     const renderPositions = button_positions.map((position, idx) => {
+      const pos = toJS(position);
+      pos[0] -= current_position[0];
+      pos[2] -= current_position[2];
       return (
         <VrButton
           key={idx}
+          onClick={(e) => {
+            const id = e.target - 6;
+            const new_position = button_positions[id];
+            global_store.current_position = new_position;
+          }}
           style={{
-            transform : [  {translate : toJS(position)}, {rotateX: 90} ],
+            transform : [  {translate : pos}, {rotateX: 90} ],
             borderRadius: 2.5,
             backgroundColor: 'black',
             height: 5,
